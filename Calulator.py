afc735b2f9031ad94f082b0e17d20e40b6fb51e7
NPT156 <70816575+NPT156@users.noreply.github.com>
2021-02-04 13:50:02 -0800

    Update Calulator.py

--- Calulator.py
+++ Calulator.py
@@ -1,42 +1,47 @@
-'''
 
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
+import time
 operator = 1
 x = 1
 y = 1
 
     
     
-    
-print("Enter first number")
-x = input()
+def calculator():    
+ print("Enter first number")
+ x = input()
 
-print("Enter operator. multiply, divide , add, or subtract.")
-operator = input()
+ print("Enter operator. 1 for multiply, 2 for divide , 3 for add")
+ print("4 for subtract, 5 for exponet")
+ operator = input()
 
-print("Enter second number")
-y = input()
+ print("Enter second number")
+ y = input()
 
-if str(operator) == "add":
-   print("Anwser:")
-   print(int(x) + int(y))
+ if str(operator) == "3":
+    print("Anwser:")
+    print(int(x) + int(y))
 
-if str(operator) == "divide":
-   print("Anwser:")
-   print(int(x) / int(y))
+ if str(operator) == "2":
+    print("Anwser:")
+    print(int(x) / int(y))
 
-if str(operator) == "multiply":
-   print("Anwser:")
-   print(int(x) * int(y) )
+ if str(operator) == "1":
+    print("Anwser:")
+    print(int(x) * int(y) )
    
-if str(operator) == "subtract":
-  print("Anwser:")
-  print(int(x) - int(y))  
+ if str(operator) == "4":
+   print("Anwser:")
+   print(int(x) - int(y)) 
    
+ if str(operator) == "5":
+   print("Anwser:")
+   print(int(x) ** int(y)) 
+ time.sleep(2)  
+  
+ print("")  
+ print("New Equation:")
+ calculator()
+calculator()   
    
    
 
