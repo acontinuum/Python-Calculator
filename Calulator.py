6dba0f8eef9e733d2b24667f94eaf919bcea945e
NPT156 <70816575+NPT156@users.noreply.github.com>
2021-02-04 11:25:22 -0800

    Create Calulator.py
    File to run

---
+++ Calulator.py
@@ -0,0 +1,47 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+operator = 1
+x = 1
+y = 1
+
+    
+    
+    
+print("Enter first number")
+x = input()
+
+print("Enter operator. multiply, divide , add, or subtract.")
+operator = input()
+
+print("Enter second number")
+y = input()
+
+if str(operator) == "add":
+   print("Anwser:")
+   print(int(x) + int(y))
+
+if str(operator) == "divide":
+   print("Anwser:")
+   print(int(x) / int(y))
+
+if str(operator) == "multiply":
+   print("Anwser:")
+   print(int(x) * int(y) )
+   
+if str(operator) == "subtract":
+  print("Anwser:")
+  print(int(x) - int(y))  
+   
+   
+   
+
+    
+    
+    
+    
+    
